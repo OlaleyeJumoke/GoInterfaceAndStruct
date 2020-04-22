8cebe260d050786e2e796a4bebe1677138c04c78
OlaleyeJumoke <olaleyesilver@gmail.com>
2020-04-22 03:12:14 -1100

    Update

--- main.go
+++ main.go
@@ -7,14 +7,25 @@ import (
 )
 
 func main() {
-	p := organization.NewPerson("Jumoke", "Olaleye")
+	p := organization.NewPerson("Jumoke", "Olaleye", organization.NewEuropeanUnionIdentifier("123456789", "Germany"))
 	fmt.Println(p)
 	fmt.Println(p.FullName())
 	err := p.SetTwitterHandler("@jumoke")
 	if err != nil {
 		fmt.Printf("An error occur while setting twitter handler %s\n", err.Error())
 	}
-	println(p.TwitterHandler())
-	println(p.TwitterHandler().RedirectURL())
+	name1 := Name{first: "Jumoke", last: "Olaleye"}
+	name2 := Name{first: "Jumoke", last: "Olaleye"}
 
+	if name1 == name2 {
+		println("we match")
+	}
+	
+ 
+}
+
+// Name strct
+type Name struct {
+	first string
+	last  string
 }
--- organization/person.go
+++ organization/person.go
@@ -3,12 +3,93 @@ package organization
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 //Identifiable interface
 type Identifiable interface {
 	ID() string
+}
+
+//Citizen interface
+type Citizen interface {
+	Country() string
+	Identifiable
+}
+
+type socialSecurityNumber string
+
+//NewSocialSecurityNumber value
+func NewSocialSecurityNumber(value string) Citizen {
+	return socialSecurityNumber(value)
+}
+
+//ID string
+func (ssn socialSecurityNumber) ID() string {
+	return string(ssn)
+}
+
+//ID string
+func (ssn socialSecurityNumber) Country() string {
+	return string(ssn)
+}
+
+type europeanUnionIdentifier struct {
+	id      string
+	country string
+}
+
+//NewEuropeanUnionIdentifier value
+// making the id an interface makes it a dynamic type(which could be anytype)
+func NewEuropeanUnionIdentifier(id interface{}, country string) Citizen {
+	switch v := id.(type) {
+	case string:
+		return europeanUnionIdentifier{
+			id:      v,
+			country: country,
+		}
+	case int:
+		return europeanUnionIdentifier{
+			id:      strconv.Itoa(v),
+			country: country,
+		}
+	case europeanUnionIdentifier:
+		return v
+	default:
+		panic("Wrong type")
+	}
+
+	//return europeanUnionIdentifier{
+	//	id:      id,
+	//	country: country,
+	//}
+}
+
+//ID string
+func (eui europeanUnionIdentifier) ID() string {
+	return eui.id
+}
+
+//ID string
+func (eui europeanUnionIdentifier) Country() string {
+	return fmt.Sprintf("Eu: %s", eui.country)
+}
+
+// Name strct
+type Name struct {
+	first string
+	last  string
+}
+
+//FullName hj
+func (n Name) FullName() string {
+	return fmt.Sprintf("Format: %s %s", n.first, n.last)
+}
+
+//Employee data
+type Employee struct {
+	Name
 }
 
 //Handler ok
@@ -29,21 +110,21 @@ func (th TwitterHandler) RedirectURL() string {
 
 //Person struct
 type Person struct {
-	firstName      string
-	lastName       string
+	Name
 	twitterHandler TwitterHandler
+	Citizen
 }
 
 // NewPerson person
-func NewPerson(lastName, firstName string) Person {
+func NewPerson(lastName, firstName string, identifiable Citizen) Person {
 	return Person{
-		firstName: firstName,
-		lastName:  lastName}
-}
+		Name: Name{
+			first: firstName,
+			last:  lastName,
+		},
+		Citizen: identifiable,
+	}
 
-//FullName hj
-func (p *Person) FullName() string {
-	return fmt.Sprintf("Format: %s %s", p.firstName, p.lastName)
 }
 
 //SetTwitterHandler jky
@@ -60,9 +141,4 @@ func (p *Person) SetTwitterHandler(handler TwitterHandler) error {
 //TwitterHandler prints the string
 func (p *Person) TwitterHandler() TwitterHandler {
 	return p.twitterHandler
-}
-
-//ID string
-func (p *Person) ID() string {
-	return "12334"
 }
